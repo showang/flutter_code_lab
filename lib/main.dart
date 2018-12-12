5f91ab134695ef11a89a2586bf96e42a550e837e
William Wang <showang730@gmail.com>
2018-12-12 11:09:23 +0800

    Fix iOS mock plugin lock issue.

--- lib/main.dart
+++ lib/main.dart
@@ -78,7 +78,7 @@ class MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
         this.isPlaying = false;
       });
     });
-    KubePlayerPlugin.currentTrack();
+//    KubePlayerPlugin.currentTrack();
   }
 
   @override
